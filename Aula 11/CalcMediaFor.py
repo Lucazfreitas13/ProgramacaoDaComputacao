e3dc21248935465d96d673c60db9a07d42bed7e8
Lucazfreitas13 <lucas.ferreiradefreitas1995@hotmail.com>
2024-04-26 21:00:55 -0300

    Aula 11

---
+++ Aula 11/CalcMediaFor.py
@@ -0,0 +1,7 @@
+media=[6, 7, 6.5, 4.8, 8]
+soma=0
+for mediaind in range (5):
+  soma= soma + media[mediaind]
+media=soma/5
+print("Média: %.2f" %media)
+
---
+++ Aula 11/Nome.py
@@ -0,0 +1,8 @@
+lista=[]
+for i in range(5):
+  nome = input("Digite um nome: ")
+  lista.append(nome)
+posi=int(input("Digite a posição do nome: "))
+print(lista[posi])
+
+  
